e1ca696a7361640640fdd8283eefe61e690974f3
vikkastaneja <vikkastaneja@users.noreply.github.com>
2024-11-01 10:03:30 -0700

    Implemented basic math for solving for Eigen values

--- linear_algebra_practice/eigen_test.py
+++ linear_algebra_practice/eigen_test.py
@@ -2,7 +2,7 @@ import sys
 
 sys.path.append('./')
 sys.path.append('linear_algebra_practice')
-from eigen_value_vector import get_one_eigen_vector, get_eigens
+from eigen_value_vector import get_one_eigen_vector, get_eigens, get_eigen_values_2d_matrix
 
 import numpy as np
 import unittest
@@ -36,9 +36,14 @@ class EigenTests(unittest.TestCase):
               [2, 3]])
 
         actual = get_eigens(A)
-  
+
         np.testing.assert_equal(actual[0][0], 5.0, "First result's values are different")
         np.testing.assert_equal(actual[1][0], 2.0, "Second result's values are different")
         np.testing.assert_array_almost_equal(actual[0][1], np.array([0.70710678, 0.70710678]), 7, "First result's arrays are different", verbose=True)
 
         np.testing.assert_array_almost_equal(actual[1][1], np.array([-0.4472136 ,  0.89442719]), 7, "Second result's arrays are different", verbose=True)
+
+    def test_get_eigen_values(self):
+        A = np.array([1, -7, 10])
+        actual = get_eigen_values_2d_matrix(A)
+        np.testing.assert_array_almost_equal(actual, np.array([5., 2.]), 7, "Eigen values are different", verbose=True)
