Slight improvement in getting the correct row for eigen vector  calculation

linear_algebra_practice/eigen_value_vector.py
@@ -63,12 +63,13 @@ def get_eigen_values_2d_matrix(coeffs):
 def get_one_eigen_vector(A, c, array_index):
     # simplify based on the eigen value
     unit_array = []
+    A_temp = A[array_index]
     if array_index == 0:
         unit_array = [1, 0]
     elif array_index == 1:
         unit_array = [0, 1]
 
-    arr = A - np.multiply(unit_array, c)
+    arr = A_temp - np.multiply(unit_array, c)
 
     tempx = -arr[1]
     tempy = arr[0]
@@ -84,7 +85,7 @@ def get_one_eigen_vector(A, c, array_index):
 
 # The following function finds the eigen vectors from the A and c above
 def get_eigen_vector(A, c):
-    return get_one_eigen_vector(A[0], c, 0)
+    return get_one_eigen_vector(A, c, 0)
 
 def get_eigens(A):
     coeffs = get_coefficients_2d_matrix(A)